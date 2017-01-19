4f7fb0140f0e220e77af61cb22b01fa800d4818e
zcphoenix2016 <13564240769@139.com>
2017-01-19 16:00:32 +0800

    theHandWithHighestValueShouldWinInTwoHandsWithOneSamePair

--- Source/PokerHands.cpp
+++ Source/PokerHands.cpp
@@ -36,7 +36,20 @@ int PokerHands::compare(vector<Card> p_cards1, vector<Card> p_cards2)
     {
         if(l_c1 == CARDS_RANK_ONE_PAIR)
         {
-            return getThePairValueFromOnePair(p_cards1) > getThePairValueFromOnePair(p_cards2) ? 1 : -1;
+            auto l_val1 = getThePairValueFromOnePair(p_cards1);
+            auto l_val2 = getThePairValueFromOnePair(p_cards2);            
+            if (l_val1 == l_val2) 
+            {
+                p_cards1.erase(remove_if(p_cards1.begin(), p_cards1.end(), 
+                          [=](auto p){return p.m_value == l_val1;}), p_cards1.end());
+                p_cards2.erase(remove_if(p_cards2.begin(), p_cards2.end(), 
+                          [=](auto p){return p.m_value == l_val1;}), p_cards2.end());
+                return findHighestVal(p_cards1) > findHighestVal(p_cards2) ? 1 : -1;          
+            }
+            else 
+            {
+                return l_val1 > l_val2 ? 1 : -1;  
+            }            
         }
         return findHighestVal(p_cards1) > findHighestVal(p_cards2) ? 1 : -1;
     }
--- Test_modules/PokerHandsTestSuite.cpp
+++ Test_modules/PokerHandsTestSuite.cpp
@@ -57,7 +57,7 @@ TEST(PokerHandsTestSuite, oneHighCardWithHighestValueShouldWinTheOtherHighCard)
     ASSERT_EQ(1, PokerHands::compare(cards2, cards1));
 }
 
-TEST(PokerHandsTestSuite, onePairCardWithHighestValueShouldWinsOnePairWithLessValue)
+TEST(PokerHandsTestSuite, onePairCardWithHighestValueShouldWinOnePairWithLessValue)
 {
     vector<Card> cards1, cards2;
     cards1.emplace_back("5H");
@@ -69,6 +69,25 @@ TEST(PokerHandsTestSuite, onePairCardWithHighestValueShouldWinsOnePairWithLessVa
     cards2.emplace_back("QH");
     cards2.emplace_back("7C");
     cards2.emplace_back("7S");
+    cards2.emplace_back("8D");
+    cards2.emplace_back("TH");
+    
+    ASSERT_EQ(-1, PokerHands::compare(cards1, cards2));
+    ASSERT_EQ(1, PokerHands::compare(cards2, cards1));
+}
+
+TEST(PokerHandsTestSuite, theHandWithHighestValueShouldWinInTwoHandsWithOneSamePair)
+{
+    vector<Card> cards1, cards2;
+    cards1.emplace_back("5H");
+    cards1.emplace_back("5C");
+    cards1.emplace_back("7S");
+    cards1.emplace_back("8D");
+    cards1.emplace_back("TH");
+    
+    cards2.emplace_back("QH");
+    cards2.emplace_back("5C");
+    cards2.emplace_back("5S");
     cards2.emplace_back("8D");
     cards2.emplace_back("TH");
     
