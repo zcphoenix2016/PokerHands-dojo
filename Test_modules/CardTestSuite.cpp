4adfcb60a00e9f54c400b6bf1ab2cf050ae16b7c
zcphoenix2016 <13564240769@139.com>
2017-02-03 10:07:11 +0800

    UT for Card

---
+++ Test_modules/CardTestSuite.cpp
@@ -0,0 +1,18 @@
+#include "gtest/gtest.h"
+#include "Card.hpp"
+
+TEST(CardTestSuite, theValueAndSuitShouldBeTheParameterOfConstructor)
+{
+    Card l_card{std::string("2H")};
+
+    ASSERT_EQ(2, l_card.value());
+    ASSERT_EQ('H', l_card.suit());
+}
+
+TEST(CardTestSuite, theCardWithLessValueShouldLoseTheComparer)
+{
+    Card l_card1{std::string("2H")}, l_card2{std::string("8S")};
+
+    ASSERT_TRUE(l_card1 < l_card2);
+    ASSERT_FALSE(l_card2 < l_card1);
+}
