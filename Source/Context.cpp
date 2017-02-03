b75565da3be63ab919e9c786c28ff0c85fc75db3
zcphoenix2016 <13564240769@139.com>
2017-02-03 09:28:50 +0800

    refactor handleFile to remove useless variable

--- Source/Context.cpp
+++ Source/Context.cpp
@@ -12,25 +12,24 @@ int Context::handleFile(const std::string& p_file)
         return -1;
     }
 
-    int l_count = 0, l_countOfCards = 0;
+    int l_count = 0;
     std::string l_card;
     std::vector<Card> l_hand1, l_hand2;
     while(not l_ifs.eof())
     {
-        l_ifs >> l_card;
-        l_countOfCards++;
+        if(5 > l_hand1.size() || 5 > l_hand2.size())
+        {
+            l_ifs >> l_card;
 
-        if(5 >= l_countOfCards)
-        {
-            l_hand1.emplace_back(l_card);
-        }
-        else
-        {
-            l_hand2.emplace_back(l_card);
-        }
+            if(5 > l_hand1.size())
+            {
+                l_hand1.emplace_back(l_card);
+            }
+            else
+            {
+                l_hand2.emplace_back(l_card);
+            }
 
-        if(10 > l_countOfCards)
-        {
             continue;
         }
 
@@ -41,7 +40,6 @@ int Context::handleFile(const std::string& p_file)
 
         l_hand1.clear();
         l_hand2.clear();
-        l_countOfCards = 0;
     }
     l_ifs.close();
 
